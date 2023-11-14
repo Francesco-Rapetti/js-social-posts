15ad0169d165d22624199cb1fa93476ea4e16184
Francesco Rapetti <rapettif80@yahoo.it>
2023-11-14 16:50:55 +0100

    feat: calculate time since a certain date

--- main.js
+++ main.js
@@ -1,57 +1,124 @@
 const posts = [
     {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+        id: 1,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/300?image=171",
+        author: {
+            name: "Phil Mangione",
+            image: "https://unsplash.it/300/300?image=15"
         },
-        "likes": 80,
-        "created": "2021-06-25"
+        likes: 80,
+        created: "2021-06-25"
     },
     {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
+        id: 2,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=112",
+        author: {
+            name: "Sofia Perlari",
+            image: "https://unsplash.it/300/300?image=10"
         },
-        "likes": 120,
-        "created": "2021-09-03"
+        likes: 120,
+        created: "2021-09-03"
     },
     {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
+        id: 3,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=234",
+        author: {
+            name: "Chiara Passaro",
+            image: "https://unsplash.it/300/300?image=20"
         },
-        "likes": 78,
-        "created": "2021-05-15"
+        likes: 78,
+        created: "2021-05-15"
     },
     {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
+        id: 4,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=24",
+        author: {
+            name: "Luca Formicola",
+            image: "https://unsplash.it/300/300?image=28"
         },
-        "likes": 56,
-        "created": "2021-04-03"
+        likes: 56,
+        created: "2021-04-03"
     },
     {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
+        id: 5,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=534",
+        author: {
+            name: "Alessandro Sainato",
+            image: "https://unsplash.it/300/300?image=29"
         },
-        "likes": 95,
-        "created": "2021-03-05"
+        likes: 95,
+        created: "2021-03-05"
     }
 ];
+const container = document.getElementById('container');
+const currentDate = new Date();
+
+postGenerator(posts[posts.length-1]);
+
+function postGenerator(post) {
+    container.innerHTML += `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="https://unsplash.it/600/300?image=171" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+}
+
+console.log(timeSince(3, 11, 2023));
+console.log(timeSince(3, 9, 2023)); 
+console.log(timeSince(3, 9, 2020)); 
+
+
+/**
+ * Calculates the time elapsed since a specific date.
+ *
+ * @param {number} day - The day of the specified date.
+ * @param {number} month - The month of the specified date.
+ * @param {number} year - The year of the specified date.
+ * @return {string} The time elapsed in the format "X giorni fa" for days less than 30, "X mesi fa" for days less than 365, and "X anni fa" for days greater than 365.
+ */
+function timeSince(day, month, year) {
+    let daySince = 
+    ((currentDate.getFullYear() - year) * 365) +
+    ((currentDate.getMonth()+1 - month) * 30) +
+    (currentDate.getDate() - day);
+
+    if (daySince < 30) {
+        return `${daySince} giorni fa`;
+    } else if (daySince < 365) {
+        return `${Math.floor(daySince/30)} mesi fa`;
+    } else {
+        return `${Math.floor(daySince/365)} anni fa`;
+    }
+}
